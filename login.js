6b282fbc935da927995f8dc5a3538b7810f0b7e2
khinthuzaraung131 <105199211+khinthuzaraung131@users.noreply.github.com>
2026-07-28 09:22:21 +0700

    Update login password from '1234' to '12345'

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 // Login function
 function login(username, password) {
-    return username === "admin" && password === "1234";
+    return username === "admin" && password === "12345";
 }
 
 // Browser code
